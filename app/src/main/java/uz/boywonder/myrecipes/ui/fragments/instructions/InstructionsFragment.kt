9944e5cbc503b64a0df657293fbbe1d0c956ddd8
boy12hoody <boy12hoody@gmail.com>
2021-07-26 14:14:55 +0500

    InstructionsFragment with WebView (Http fix in Manifest). UI improvements in fragment_overview.xml and recipes_row_layout.xml

--- app/src/main/java/uz/boywonder/myrecipes/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/uz/boywonder/myrecipes/ui/fragments/instructions/InstructionsFragment.kt
@@ -1,21 +1,40 @@
 package uz.boywonder.myrecipes.ui.fragments.instructions
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.viewbinding.library.fragment.viewBinding
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import uz.boywonder.myrecipes.R
+import uz.boywonder.myrecipes.databinding.FragmentInstructionsBinding
+import uz.boywonder.myrecipes.models.Result
+import uz.boywonder.myrecipes.util.Constants
 
 
-class InstructionsFragment : Fragment() {
+class InstructionsFragment : Fragment(R.layout.fragment_instructions) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+    private val binding: FragmentInstructionsBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val args = arguments
+        val myBundle: Result = args!!.getParcelable<Result>(Constants.RECIPE_RESULT_KEY) as Result
+
+        binding.apply {
+            ingredientsWebView.webViewClient = object : WebViewClient() {}
+
+            if (myBundle.sourceUrl != null) {
+                ingredientsWebView.loadUrl(myBundle.sourceUrl)
+            } else {
+                Snackbar.make(
+                    requireView(), "No Website Found For Instructions.", Snackbar.LENGTH_SHORT
+                ).show()
+            }
+
+        }
+
     }
 
 }
